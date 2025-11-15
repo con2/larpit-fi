8a47643a6f753da5110c7b7e3cdad980480bd933
Luka Pajukanta <santtu@pajukanta.fi>
2025-11-15 14:45:30 +0200

    fix: code alert for incomplete URL substring sanitization

--- src/models/LarpLink.ts
+++ src/models/LarpLink.ts
@@ -70,23 +70,24 @@ export function formToLarpLinks(form: LarpLinksForm): LarpLinkUpsertable[] {
 export function socialMediaLinkTitleFromHref(href: string) {
   href = href.trim();
 
-  if (href.startsWith("https://instagram.com")) {
+  // NOTE none of these should point to the root of the service
+  if (href.startsWith("https://instagram.com/")) {
     return "Instagram";
   } else if (
-    href.startsWith("https://x.com") ||
-    href.startsWith("https://twitter.com")
+    href.startsWith("https://x.com/") ||
+    href.startsWith("https://twitter.com/")
   ) {
     return "Twitter";
   } else if (
-    href.startsWith("https://facebook.com") ||
-    href.startsWith("https://fb.me")
+    href.startsWith("https://facebook.com/") ||
+    href.startsWith("https://fb.me/")
   ) {
     return "Facebook";
-  } else if (href.startsWith("https://discord.gg")) {
+  } else if (href.startsWith("https://discord.gg/")) {
     return "Discord";
   } else if (
-    href.startsWith("https://youtu.be") ||
-    href.startsWith("https://www.youtube.com")
+    href.startsWith("https://youtu.be/") ||
+    href.startsWith("https://www.youtube.com/")
   ) {
     return "YouTube";
   }
