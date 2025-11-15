5908561e82ff890c7905d2c48be17ebf7216ce17
Luka Pajukanta <santtu@pajukanta.fi>
2025-11-15 10:37:02 +0200

    feat: thanks page for edit larp

---
+++ src/app/[locale]/larp/[larpId]/edit/thanks/page.tsx
@@ -0,0 +1,36 @@
+import MainHeading from "@/components/MainHeading";
+import { getTranslations } from "@/translations";
+import Link from "next/link";
+import { Card, CardBody, CardTitle } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+
+interface Props {
+  params: Promise<{
+    locale: string;
+  }>;
+}
+
+export default async function NewLarpThanksPage({ params }: Props) {
+  const { locale } = await params;
+
+  const translations = getTranslations(locale);
+  const t = translations.NewLarpPage;
+  const ediT = translations.EditLarpPage;
+
+  return (
+    <Container>
+      <MainHeading>{ediT.title}</MainHeading>
+      <Card>
+        <CardBody>
+          <CardTitle>{t.subpages.thanks.title}</CardTitle>
+          {t.subpages.thanks.message}
+          <div>
+            <Link className="btn btn-primary mt-3" href="/">
+              {t.subpages.thanks.backToFrontPage}
+            </Link>
+          </div>
+        </CardBody>
+      </Card>
+    </Container>
+  );
+}
