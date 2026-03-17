eb93b1d8005caa03a8f8374322aee9df37372761
Luka Pajukanta <santtu@pajukanta.fi>
2026-03-17 19:36:48 +0200

    chore: silence warning about tsconfigPaths

--- vitest.integration.config.ts
+++ vitest.integration.config.ts
@@ -1,10 +1,10 @@
 import "dotenv/config";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [react()],
+  resolve: { tsconfigPaths: true },
   test: {
     environment: "node",
     include: ["src/**/*.integration.test.ts"],
